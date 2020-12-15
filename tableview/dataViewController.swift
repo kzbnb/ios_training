68d0a0040d5a4c60f5bf56c98132a1007b48a2db
kzbnb <51832217+kzbnb@users.noreply.github.com>
2020-12-15 12:16:14 +0800

    成功获取经纬度，获取城市失败

--- tableview/dataViewController.swift
+++ tableview/dataViewController.swift
@@ -7,9 +7,11 @@
 //
 
 import UIKit
+import CoreLocation
 
-class dataViewController: UIViewController ,UINavigationControllerDelegate,UIImagePickerControllerDelegate{
+class dataViewController: UIViewController ,UINavigationControllerDelegate,CLLocationManagerDelegate,UIImagePickerControllerDelegate{
 
+    var locationManager:CLLocationManager!
 
     @IBAction func tapForPhoto(_ sender: Any) {
         let imagePicker=UIImagePickerController()
@@ -45,6 +47,34 @@ class dataViewController: UIViewController ,UINavigationControllerDelegate,UIIma
             self.phoneNumText.text=contact.phone;
             self.avatarLabel.image=contact.avatar;
         }
+        
+        locationManager = CLLocationManager()
+        
+                 // 设置定位的精确度
+                 locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        
+                 // 设置定位变化的最小距离 距离过滤器
+                 locationManager.distanceFilter = 50
+        
+                 // 设置请求定位的状态
+                 if #available(iOS 8.0, *) {
+                         locationManager.requestWhenInUseAuthorization()
+                     } else {
+                         // Fallback on earlier versions
+                         print("hello")
+                     }//这个是在ios8之后才有的
+        
+                 // 设置代理为当前对象
+                 locationManager.delegate = self;
+        
+                 if CLLocationManager.locationServicesEnabled(){
+                         // 开启定位服务
+                         locationManager.startUpdatingLocation()
+                     }else{
+                         print("没有定位服务")
+                     }
+        
+        
         // Do any additional setup after loading the view.
     }
     
@@ -67,6 +97,51 @@ class dataViewController: UIViewController ,UINavigationControllerDelegate,UIIma
         self.avatarLabel.image=selectedImage
         dismiss(animated: true, completion: nil)
     }
+    
+    lazy var geoCoder: CLGeocoder = {
+        return CLGeocoder()
+    }()
+    
+    // 定位失败调用的代理方法
+    func locationManager(manager: CLLocationManager, didFailWithError error: Error) {
+                 print(error)
+             }
+    
+    var locationInfoAll:CLLocation!
+         // 定位更新地理信息调用的代理方法
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+                 if locations.count > 0
+                 {
+                            locationInfoAll=locations.last!
+                         let locationInfo = locations.last!
+                         let alert:UIAlertView = UIAlertView(title: "获取的地理坐标",
+                                                                                message: "经度是：\(locationInfo.coordinate.longitude)，维度是：\(locationInfo.coordinate.latitude)",
+                                 delegate: nil, cancelButtonTitle: "是的")
+                         alert.show()
+                    reverseGeoCode()
+                     }
+             }
+    
+    
+    func reverseGeoCode() {
+        let latitude = CLLocationDegrees(locationInfoAll.coordinate.latitude)
+        let longitude = CLLocationDegrees(locationInfoAll.coordinate.longitude)
+    let loc1 = CLLocation(latitude: latitude, longitude: longitude)
+    geoCoder.reverseGeocodeLocation(loc1) { (pls: [CLPlacemark]?, error: Error?)  in
+    if error == nil {
+    print("反地理编码成功")
+    guard let plsResult = pls else {return}
+    let firstPL = plsResult.first
+        let alert:UIAlertView = UIAlertView(title: firstPL?.name,
+                                            message:firstPL?.name,
+            delegate: nil, cancelButtonTitle: "是的")
+        alert.show()
+    }else {
+    print("错误")
+    }
+    }
+    }
+    
  
 
 }
