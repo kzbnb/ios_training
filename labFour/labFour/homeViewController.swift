b394948f04d6dc0c123eafe5a9d120429b3ffd84
kzbnb <51832217+kzbnb@users.noreply.github.com>
2020-12-16 20:09:40 +0800

    摇晃手机出鸡汤 换桌面

--- labFour/labFour/homeViewController.swift
+++ labFour/labFour/homeViewController.swift
@@ -16,6 +16,39 @@ class homeViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
     
+    
+    override func motionBegan(_ motion: UIEvent.EventSubtype, with event: UIEvent?) {
+        print("开始摇晃")
+    }
+    
+    override func motionEnded(_ motion: UIEvent.EventSubtype, with event: UIEvent?) {
+        print("摇晃结束")
+        var a=Int(arc4random() % 3)
+        switch a {
+        case 0  :
+            showMsgbox(_message: "鸡汤1",_title:"鸡汤title")
+        case 1  :
+            showMsgbox(_message: "鸡汤2",_title:"鸡汤title")
+        case 2  :
+            showMsgbox(_message: "鸡汤3",_title:"鸡汤title")
+        default :
+            showMsgbox(_message: "鸡汤4",_title:"鸡汤title")
+        }
+        
+    }
+    
+    override func motionCancelled(_ motion: UIEvent.EventSubtype, with event: UIEvent?) {
+        print("摇晃被意外终止")
+    }
+    
+    func showMsgbox(_message: String, _title: String ){
+        
+        let alert = UIAlertController(title: _title, message: _message, preferredStyle: UIAlertController.Style.alert)
+        let btnOK = UIAlertAction(title: "好的", style: .default, handler: nil)
+        alert.addAction(btnOK)
+        self.present(alert, animated: true, completion: nil)
+        
+    }
 
     /*
     // MARK: - Navigation
--- labFour/labFour/myDataViewCell.swift
+++ labFour/labFour/myDataViewCell.swift
@@ -25,5 +25,7 @@ class myDataViewCell: UITableViewCell {
 
         // Configure the view for the selected state
     }
+    
+   
 
 }
